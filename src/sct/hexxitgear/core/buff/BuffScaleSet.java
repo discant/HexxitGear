23429dfa0cf29b09836297d7c5f04bede1d78dc1
Xander Smeets <xsmeets@discant.net>
2015-01-30 15:32:38 +0100

    Update BuffScaleSet.java

--- src/sct/hexxitgear/core/buff/BuffScaleSet.java
+++ src/sct/hexxitgear/core/buff/BuffScaleSet.java
@@ -14,6 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * File changed by Xander Smeets, 30-1-2015.
  */
 
 package sct.hexxitgear.core.buff;
